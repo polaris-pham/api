133fef632182476eb89bddda25b2ea1284f3fe39
polaris <haian.phamnguyen@gmail.com>
2022-05-26 14:38:58 +0700

    End Section 2

---
+++ TestAPI/TestAPI/Data/APIDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TestAPI.Models.Domain;
+
+namespace TestAPI.Data
+{
+    public class APIDbContext: DbContext 
+    {
+        public APIDbContext(DbContextOptions<APIDbContext> options): base(options)
+        {
+
+        }
+
+        public DbSet<Region> Regions { get; set; }
+
+        public DbSet<Walk> Walks { get; set; }
+
+        public DbSet<WalkDifficulty> WalkDifficulty { get; set; }
+
+
+    }
+}
---
+++ TestAPI/TestAPI/Models/Domain/Region.cs
@@ -0,0 +1,25 @@
+﻿namespace TestAPI.Models.Domain
+{
+    public class Region
+    {
+        public Guid Id { get; set; }
+        public string Code { get; set; }
+
+        public string Name { get; set; }
+
+
+        public double Area { get; set; }
+
+        public double Lat { get; set; }
+
+        public double Long { get; set; }
+
+        public long Population { get; set; }
+
+        // Navigation Property 
+        public IEnumerable<Walk> Walks { get; set; } // 1 Region co the co nhieu Walk // khai bao cho Entity FrameWork
+
+
+
+    }
+}
