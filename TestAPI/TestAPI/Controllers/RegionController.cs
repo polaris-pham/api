1c265489e4c8cbb6a40fb077b53eef24057f8084
gfg-gis <info@greenfeed.com.vn>
2022-05-29 11:52:50 +0700

    End Session 2

---
+++ TestAPI/TestAPI/Controllers/RegionController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TestAPI.Models.Domain;
+
+namespace TestAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+
+    public class RegionController : Controller
+    {
+        [HttpGet]
+        public IActionResult GetAllRegions()
+        {
+            var regions = new List<Region>()
+            {
+                new Region
+                {
+                    Id = Guid.NewGuid(),
+                    Name ="Saigon",
+                    Code = "SG",
+                    Area =  121212,
+                    Lat = -12212,
+                    Long = 333332,
+                    Population = 21323131
+                },
+
+                new Region
+                {
+                    Id = Guid.NewGuid(),
+                    Name ="Ha Noi",
+                    Code = "HN",
+                    Area =  121212,
+                    Lat = -12212,
+                    Long = 333332,
+                    Population = 21323131
+                }
+
+
+            };
+
+            return Ok(regions);
+        }
+       
+    }
+}
